07b26d01b70a8a95704db6274e0280da23c86052
Andre Mapa <andreomapa01@gamil.com>
2022-01-18 11:16:09 -0300

    Project created

---
+++ src/main/java/com/andreMapa/projectspringBootmogoDB/ProjectSpringBootMogoDbApplication.java
@@ -0,0 +1,13 @@
+package com.andreMapa.projectspringBootmogoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootMogoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootMogoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreMapa/projectspringBootmogoDB/ProjectSpringBootMogoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreMapa.projectspringBootmogoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBootMogoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
